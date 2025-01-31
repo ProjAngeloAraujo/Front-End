f7d2a237158afedf157b6dd4d873e685876d1783
ProjAngeloAraujo <angelo.souza23@etec.sp.gov.br>
2025-01-31 19:30:55 -0300

    dia 17

--- NIVEL-1/JS/Aula17/app.js
+++ NIVEL-1/JS/Aula17/app.js
@@ -1,38 +1,63 @@
 let button = document.getElementById('button');
 let input = document.getElementById('input');
 let divItens = document.getElementById('divItens');
-let buttonDelete = document.getElementById('buttonDelete');
+let itens = [];
 
 button.addEventListener('click', Create);
 
 function Create(){
     let inputvalue = input.value;
 
-    let divItemContainer = document.createElement('div');
-    divItemContainer.id = 'divItemContainer';
+    if(inputvalue === "" || inputvalue === undefined || inputvalue === null){
+        return 0;
+    }
 
-    let paragrafo = document.createElement('p');
-    paragrafo.textContent = inputvalue;
-    paragrafo.id = 'paragrafo';
-    divItemContainer.appendChild(paragrafo);
-    
-    let buttonDelete = document.createElement('button');
-    buttonDelete.textContent = 'excluir';
-    buttonDelete.addEventListener('click', () => Delete(divItemContainer));
-    divItemContainer.appendChild(buttonDelete);
+    itens.push(inputvalue);
 
-    divItens.appendChild(divItemContainer);
-    
+    Read();
+    input.value = '';
 }
 
 function Read(){
+    divItens.innerHTML = "";
 
+    itens.forEach(function(item, index){
+
+        let divItemContainer = document.createElement('div');
+        divItemContainer.className = 'divItemContainer';
+
+        let paragrafo = document.createElement('p');
+        paragrafo.textContent = item;
+        paragrafo.className = 'paragrafo';
+        divItemContainer.appendChild(paragrafo);
+        
+        let buttonDelete = document.createElement('button');
+        buttonDelete.textContent = 'excluir';
+        buttonDelete.className = 'buttonDelete';
+        buttonDelete.addEventListener('click', () => Delete(index));
+        divItemContainer.appendChild(buttonDelete);
+
+        let buttonUpdate = document.createElement('button');
+        buttonUpdate.textContent = 'editar';
+        buttonUpdate.className = 'buttonUpdate';
+        buttonUpdate.addEventListener('click', () => Update(index));
+        divItemContainer.appendChild(buttonUpdate);
+
+        divItens.appendChild(divItemContainer);
+    });
+    
 }
 
-function Update(){
-
+function Update(index){
+    let novoValor = prompt("Digite o novo Valor:");
+    if(novoValor === "" || novoValor === undefined || novoValor === null){
+        return 0;
+    }
+    itens.splice(index, 1, novoValor)
+    Read();
 }
 
-function Delete(item){
-    divItens.removeChild(item);
+function Delete(index){
+    itens.splice(index, 1);
+    Read();
 }
